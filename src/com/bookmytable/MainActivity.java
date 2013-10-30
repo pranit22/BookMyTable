a3331da912daed0330d5812fad19c2fc89e9d4f4
Pranit Mhatre <pranit22@gmail.com>
2013-10-29 20:17:12 -0700

    Designed LoginActivity

--- src/com/bookmytable/MainActivity.java
+++ src/com/bookmytable/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bookmytable;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -12,6 +14,8 @@ public class MainActivity extends Activity
     {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	startActivity(new Intent(this, LoginActivity.class));
+
     }
 
     @Override
@@ -20,6 +24,11 @@ public class MainActivity extends Activity
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.main, menu);
 	return true;
+    }
+
+    public void navigateToLoginActivity(View view)
+    {
+	startActivity(new Intent(this, LoginActivity.class));
     }
 
 }
