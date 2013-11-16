6928059c60440862031ab42474fa7a1f6b9fb083
Pranit Mhatre <pranit22@gmail.com>
2013-11-16 13:17:07 -0800

    Activity Booking

--- src/com/bookmytable/BookingActivity.java
+++ src/com/bookmytable/BookingActivity.java
@@ -1,8 +1,10 @@
 package com.bookmytable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class BookingActivity extends Activity
 {
@@ -20,6 +22,16 @@ public class BookingActivity extends Activity
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.booking, menu);
 	return true;
+    }
+
+    public void navigateToMenuActivity (View view)
+    {
+        startActivity(new Intent(this, MenuActivity.class));
+    }
+
+    public void navigateToTableSelectionActivity (View view)
+    {
+        startActivity(new Intent(this, TableSelectionActivity.class));
     }
 
 }
