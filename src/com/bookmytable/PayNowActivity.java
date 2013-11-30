69c35bbf374ce3fbd1a16851361000c4d07b56e3
Pranit Mhatre <pranit22@gmail.com>
2013-11-29 21:43:35 -0800

    Getting amount from tab

--- src/com/bookmytable/PayNowActivity.java
+++ src/com/bookmytable/PayNowActivity.java
@@ -1,22 +1,54 @@
 package com.bookmytable;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PayNowActivity extends Activity {
+
+    NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_now);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.pay_now, menu);
-        return true;
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+
+        String amount = new String(msg.getRecords()[0].getPayload());
+        ((TextView)findViewById(R.id.amount)).setText(amount);;
     }
 
 }
